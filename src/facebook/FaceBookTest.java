b5f1211090fcbdd22984149fd771803578d01378
Jakub Gajda <jgajda@jitsolutions.pl>
2015-02-24 22:25:43 +0100

    Dodanie http component

--- src/facebook/FaceBookTest.java
+++ src/facebook/FaceBookTest.java
@@ -2,6 +2,7 @@ package facebook;
 
 import java.util.List;
 
+import org.apache.http.client.fluent.Request;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -9,6 +10,8 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
+
+import com.thoughtworks.selenium.webdriven.commands.IsElementPresent;
 
 public class FaceBookTest {
 
@@ -18,6 +21,14 @@ public class FaceBookTest {
 	public void before(){
 		System.setProperty("webdriver.chrome.driver", "C:\\BEST\\chromedriver_win32\\chromedriver.exe");
 		driver = new ChromeDriver();
+		
+		String facebook = "https://www.facebook.com/";
+		
+		try{
+			System.out.println(Request.Get(facebook).execute().returnResponse().);
+		}catch(Exception e){
+			System.out.println(e);
+		}
 	}
 	@Test
 	public void testFaceBook() throws InterruptedException{
@@ -25,6 +36,7 @@ public class FaceBookTest {
 		List<WebElement> lista = driver.findElements(By.xpath("//input[@type='text' and @class='inputtext _58mg _5dba _2ph-']"));
 		for(WebElement el:lista){
 			el.sendKeys("23214324324");
+			
 			Thread.sleep(3000);
 		}
 	}
